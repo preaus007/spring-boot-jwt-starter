b74970eb62bc28f74a748e338969c12945084c27
preaus007 <touhedcs@gmail.com>
2026-03-02 12:46:17 +0600

    Add CloudinaryConfig

---
+++ src/main/java/com/touhed/app/config/CloudinaryConfig.java
@@ -0,0 +1,24 @@
+package com.touhed.app.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+    @Value("${cloudinary.cloud_name}") String cloudName;
+    @Value("${cloudinary.api_key}") String apiKey;
+    @Value("${cloudinary.api_secret}") String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary( ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true
+        ) );
+    }
+}
